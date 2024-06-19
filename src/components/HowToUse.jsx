3c621f3dd6750c09a404e40f96288cbffdd15c01
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-19 11:15:37 +0300

    + facial id desk

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -34,6 +34,33 @@ const HowToUse = () => {
           </ul>
         </div>
       </div>
+
+      <div className="relative mt-[90px] flex h-[470px] flex-col items-center justify-center bg-[url('assets/facial-id.png')] bg-center px-5 md:ml-[94px] md:mr-5 md:px-0">
+        <div className="absolute left-[30px] w-full rounded-lg bg-white bg-opacity-90 px-7 lg:w-[390px] lg:px-5 lg:py-[30px]">
+          <h2 className="mb-3 font-space-grotesk text-xl font-semibold lg:mb-8 lg:pt-2 lg:text-[33px]">
+            Facial identification
+          </h2>
+          <p className="font-darker-grotesque text-[15px] leading-[1.5] text-[#071a56] lg:mb-0 lg:text-lg">
+            Effortlessly upload your photos and let the platform accurately
+            identify faces, streamlining organization and enhancing viewing
+            experience.
+          </p>
+          <ul className="space-y-3 pt-4 font-space-grotesk text-[15px] lg:pt-6 lg:text-xl">
+            <li className="flex items-center justify-start gap-3">
+              <img className="w-4 lg:w-auto" src={Checkmark} alt="" /> Instant
+              facial recognition
+            </li>
+            <li className="flex items-center justify-start gap-3">
+              <img className="w-4 lg:w-auto" src={Checkmark} alt="" />{" "}
+              Personalized photo organization
+            </li>
+            <li className="flex items-center justify-start gap-3">
+              <img className="w-4 lg:w-auto" src={Checkmark} alt="" /> Enhanced
+              privacy controls
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
