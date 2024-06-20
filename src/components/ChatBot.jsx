e0b9edbeac652744c716eb237bc69538389a5a4e
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-20 11:26:10 +0300

    + ai chatbot mobile

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -7,10 +7,10 @@ const ChatBot = () => {
   return (
     <section className="relative mt-[75px] lg:mt-[182px]" id="chatbot ">
       {/* Grouping images */}
-      <div className="flex h-[400px] flex-col bg-[url('assets/ai-section-bg.png')] bg-center md:mt-[90px] md:h-[569px] md:items-center md:justify-center">
-        <div className="left-[124px] top-[73px] mx-10 mt-20 rounded-lg bg-opacity-90 py-3 text-white md:absolute md:mx-0 md:mt-[55px]">
+      <div className="flex h-[722px] flex-col bg-[url('assets/ai-section-bg.png')] bg-center md:mt-[90px] md:h-[569px] md:items-center md:justify-center">
+        <div className="left-[124px] top-[73px] mx-5 mt-5 py-3 text-white md:absolute md:mx-0 md:mt-[55px]">
           {/* header */}
-          <div className="flex w-[415px] flex-col items-center justify-center p-0 px-5 md:px-0">
+          <div className="flex flex-col items-center justify-center p-0 md:w-[415px] md:px-0">
             <div className="mt-[18px] flex items-center justify-center gap-2 md:mt-0">
               <img src={UnionL} alt="" />
               <h5 className="text-center font-darker-grotesque text-[10px] font-bold leading-[1.5] md:mt-0 md:text-lg">
@@ -19,19 +19,19 @@ const ChatBot = () => {
               <img src={UnionR} alt="" />
             </div>
             {/* text */}
-            <h2 className="relative pt-[2px] text-center font-space-grotesk text-[26px] font-semibold leading-[120%] md:pt-0 md:text-left md:text-[40px]">
+            <h2 className="relative ml-8 font-space-grotesk text-[26px] font-semibold leading-[120%] md:ml-0 md:pt-0 md:text-[40px]">
               <span className="bg-clip-text text-[#21e8fd] text-transparent">
                 AI Chatbot:
               </span>{" "}
               Natural Language Interaction
             </h2>
-            <p className="mt-[14px] text-center font-darker-grotesque text-[12px] leading-[150%] md:mt-[24px] md:text-left md:text-lg">
+            <p className="mx-8 mt-[14px] font-darker-grotesque text-[15px] leading-[150%] md:mx-0 md:mt-[24px] md:text-lg">
               Engage in seamless conversation with our AI chatbot, using natural
               language to execute search commands effortlessly.
             </p>
           </div>{" "}
-          <div className="mt-7 flex gap-8">
-            <button className="flex h-[57px] w-full items-center justify-center rounded-lg bg-white px-[15px] py-[12px] font-darker-grotesque text-sm font-bold text-[#030610] md:w-[187px] md:justify-between md:px-[24px] md:py-[14px] md:text-lg">
+          <div className="mt-5 flex gap-8 pl-8 md:mt-7 md:pl-0">
+            <button className="flex h-[45px] w-[161px] items-center justify-center rounded-lg bg-white px-[15px] py-[12px] font-darker-grotesque text-sm font-bold text-[#030610] md:h-[57px] md:w-[187px] md:justify-between md:px-[24px] md:py-[14px] md:text-lg">
               Schedule a demo
               <svg
                 width="16.000000"
@@ -52,11 +52,15 @@ const ChatBot = () => {
                 />
               </svg>
             </button>
-            <img className="-mt-[60px]" src={Arrow} alt="" />
+            <img
+              className="h-[40px] w-[50px] md:-mt-[60px] md:h-auto md:w-auto"
+              src={Arrow}
+              alt=""
+            />
           </div>
         </div>
 
-        <div className="absolute left-[711px] top-[60px]">
+        <div className="left-[711px] top-[60px] m-5 md:absolute">
           <img src={Devices} alt="" />
         </div>
       </div>
