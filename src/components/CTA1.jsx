41f7ec68d5fe9652e9d073b7b0335c66358256a5
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-21 13:08:02 +0300

    + cta1 mobile

--- src/components/CTA1.jsx
+++ src/components/CTA1.jsx
@@ -5,12 +5,12 @@ import Arrow from "../assets/arrow-cta1.svg";
 
 const CTA1 = () => {
   return (
-    <section className="mt-[260px]" id="cta1">
-      <div className="relative mt-[60px] flex h-[400px] flex-col bg-[url('assets/CTA1.png')] bg-center md:mt-[90px] md:h-[698px] md:items-center md:justify-center">
-        <div className="left-[124px] top-[110px] rounded-lg bg-white bg-opacity-90 md:absolute md:p-10 lg:h-[478px] lg:w-[527px]">
+    <section className="mt-[80px] md:mt-[260px]" id="cta1">
+      <div className="relative mt-[60px] flex h-[560px] flex-col items-center bg-[url('assets/CTA1.png')] md:mt-[90px] md:h-[698px] md:justify-center md:bg-center">
+        <div className="left-[124px] top-[110px] mt-[105px] h-[347px] w-[291px] rounded-md bg-white bg-opacity-90 p-[10px] md:absolute md:mx-0 md:mt-0 md:rounded-lg md:p-10 lg:h-[478px] lg:w-[527px]">
           {/* header */}
-          <div className="flex flex-col items-center justify-center">
-            <div className="mt-[18px] flex items-center justify-center gap-2 md:mr-3 md:mt-0">
+          <div className="flex flex-col items-center justify-center pt-1 md:pt-0">
+            <div className="flex items-center justify-center gap-2 md:mr-3 md:mt-0">
               <img src={UnionL} alt="" />
               <h5 className="text-center font-darker-grotesque text-[10px] font-bold leading-[1.5] text-[#030610] md:mt-0 md:text-lg">
                 ASSISTANCE
@@ -18,34 +18,34 @@ const CTA1 = () => {
               <img src={UnionR} alt="" />
             </div>
             {/* text */}
-            <h2 className="relative self-start pt-[2px] font-space-grotesk text-[26px] font-semibold leading-[120%] md:pt-0 md:text-[40px]">
+            <h2 className="relative ml-1 self-start font-space-grotesk text-[26px] font-semibold leading-[120%] md:ml-0 md:text-[40px]">
               <span className="bg-gradient-to-r from-purple-600 to-blue-900 bg-clip-text text-transparent">
                 Still have
               </span>{" "}
               questions?
             </h2>
-            <p className="mt-[14px] font-darker-grotesque text-[15px] leading-[150%] text-[#081A56] md:mt-[18px] md:text-lg">
+            <p className="ml-1 mt-[10px] font-darker-grotesque text-[15px] leading-[150%] text-[#081A56] md:ml-0 md:mt-[18px] md:text-lg">
               If you still have questions, you can join a quick 30-minute
               demonstration session on how VIXC, empowered by AI technology,
               will help you search and organize your photo collections.
             </p>
           </div>
-          <ul className="space-y-3 pt-4 font-space-grotesk text-[15px] lg:pt-[24px] lg:text-xl">
-            <li className="flex items-center justify-start gap-4">
+          <ul className="space-y-3 px-2 pt-4 font-space-grotesk text-[15px] md:px-0 lg:pt-[24px] lg:text-xl">
+            <li className="flex items-center justify-start gap-3 md:gap-4">
               <img className="w-4 lg:w-auto" src={Checkmark} alt="" /> Choose
               any date and time
             </li>
-            <li className="flex items-center justify-start gap-4">
+            <li className="flex items-center justify-start gap-3 md:gap-4">
               <img className="w-4 lg:w-auto" src={Checkmark} alt="" /> It&apos;s
               completely free
             </li>
-            <li className="flex items-center justify-start gap-4">
+            <li className="flex items-center justify-start gap-3 md:gap-4">
               <img className="w-4 lg:w-auto" src={Checkmark} alt="" /> Get
               expert assistance
             </li>
           </ul>
 
-          <div className="mt-5 flex gap-8 pl-8 md:mt-7 md:pl-0">
+          <div className="mt-5 flex gap-8 px-1 md:mt-7 md:px-0">
             <button className="flex h-[45px] w-[161px] items-center justify-center rounded-lg bg-gradient-to-r from-[#8D1EA2] to-[#090EDB] px-[15px] py-[12px] font-darker-grotesque text-sm font-bold text-white md:h-[57px] md:w-[187px] md:justify-between md:px-[24px] md:py-[14px] md:text-lg">
               Schedule a demo
               <svg
